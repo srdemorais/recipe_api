3dc9491baba78ae3aca640c18f612d28677532e2
Sergio de Morais <srdemorais.sandbox@gmail.com>
2025-06-28 00:45:03 +0200

    Remove redundant solid_cache/queue connects_to in production.rb after database.yml fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,14 +46,14 @@ Rails.application.configure do
   # Replace the default in-process memory cache store with a durable alternative.
   config.cache_store = :solid_cache_store
   # Explicitly configure Solid Cache to use the primary database connection
-  config.solid_cache.connects_to = { database: { writing: :primary, reading: :primary } }
+  # config.solid_cache.connects_to = { database: { writing: :primary, reading: :primary } }
 
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
   # config.solid_queue.connects_to = { database: { writing: :queue } }
   # Explicitly configure Solid Queue to use the primary database connection
-  config.solid_queue.connects_to = { database: { writing: :primary, reading: :primary } }
+  # config.solid_queue.connects_to = { database: { writing: :primary, reading: :primary } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
